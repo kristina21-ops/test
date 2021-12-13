a690fd148ab4894a6425200928b26054db248652
Kristina <64296866+kristina21-ops@users.noreply.github.com>
2021-12-13 23:48:20 +0600

    test
    test

---
+++ teacher/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Course(models.Model):
+   course_name = models.CharField(max_length=50)
+   question_number = models.PositiveIntegerField()
+   total_marks = models.PositiveIntegerField()
+   def __str__(self):
+        return self.course_name
+        
+class Teacher(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/Teacher/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    status= models.BooleanField(default=False)
+    salary=models.PositiveIntegerField(null=True)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_instance(self):
+        return self
+    def __str__(self):
+        return self.user.first_name
+
